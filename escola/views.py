01fbcb118e92eccbfd0aaacf6034de954950bed4
Heveraldo <heveraldob12@gmail.com>
2023-02-18 09:52:20 -0300

    first commit

---
+++ escola/views.py
@@ -0,0 +1,17 @@
+from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso,Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
+class MatriculasViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
